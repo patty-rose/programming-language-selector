c1c6ffa7082a51ba1419bba3ccf87d11ce67b26f
patty-rose <oteropatty@gmail.com>
2022-04-09 13:21:22 -0700

    finish if/else scripts, update button, fix Name span element

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,27 @@ jQuery(document).ready(function() {
 
     const name = $("input#name").val();
     const langGuess = $("input#langGuess").val();
+    // if langGuess contains name of language the survey returned. then return "congrats, your guess was right!!" else return Sorry, you should not learn "input", you should learn "PYTHON!"
     const risk = $("#risk").val();
     const dog = $("#dog").val();
     const color = $("#color").val();
+    const lang = "";
 
-    if(risk === 1 || 2 && dog === 1 || 2) {
+    console.log(name);
+    console.log(langGuess);
+    console.log(color);
+
+    if(risk === "1" && dog === "1" || dog === "2") {
+      lang = "javascript";
       $("#javascript").show();
-    } else if (risk === 3 && dog === 3) {
+    } else if (risk === "2" && dog === "3" || risk === "3") {
+      lang = "python";
       $("#python").show();
     } else {
+      lang = "rust";
       $("#rust").show();
     }
+    
+    $(".name").text(name + ", you should learn Javascript!");
   });
 })
