adee4cd708d6de821c6ab282b236c8c08d53fca5
patty-rose <oteropatty@gmail.com>
2022-04-08 17:52:53 -0700

    WIP form submit scripts and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function() {
-  
+  $("form#language").submit(function(event) {
+    event.preventDefault();
+
+    $("#language").hide();
+    $("#javascript").hide();
+    $("#python").hide();
+    $("#rust").hide();
+
+    const name = $("input#name").val();
+    const langGuess = $("input#langGuess").val();
+    const risk = $("#risk").val();
+    const dog = $("#dog").val();
+    const color = $("#color").val();
+
+    if(risk === 1 || 2 && dog === 1 || 2) {
+      $("#javascript").show();
+    } else if (risk === 3 && dog === 3) {
+      $("#python").show();
+    } else {
+      $("#rust").show();
+    }
+  });
 })
