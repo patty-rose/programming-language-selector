2e13c1bacb6a3f7d026d406ab66d0e767d0520d8
patty-rose <oteropatty@gmail.com>
2022-04-09 13:50:36 -0700

    WIP languageCheck function

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
+function languageCheck(langGuess, lang) {
+  let langCheck = ""
+  let strCheck = langGuess.search(/lang/i);
+  if (strCheck === -1) {
+    langCheck = "false";
+  } else langCheck = "true";
+};
+
 jQuery(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
-
     $("#language").hide();
     $("#javascript").hide();
     $("#python").hide();
     $("#rust").hide();
-
     const name = $("input#name").val();
     const langGuess = $("input#langGuess").val();
-    // if langGuess contains name of language the survey returned. then return "congrats, your guess was right!!" else return Sorry, you should not learn "input", you should learn "PYTHON!"
     const risk = $("#risk").val();
     const dog = $("#dog").val();
     const color = $("#color").val();
-    const lang = "";
+    let lang = "";
 
-    console.log(name);
-    console.log(langGuess);
-    console.log(color);
-
-    if(risk === "1" && dog === "1" || dog === "2") {
+    if (risk === "1" && dog === "1" || dog === "2") {
       lang = "javascript";
       $("#javascript").show();
     } else if (risk === "2" && dog === "3" || risk === "3") {
-      lang = "python";
+      lang = "python"
       $("#python").show();
     } else {
       lang = "rust";
       $("#rust").show();
-    }
-    
-    $(".name").text(name + ", you should learn Javascript!");
+    };
+
+    $(".name").text(name + ", you should learn " + lang + "!");
+    console.log(langGuess);
+    console.log(languageCheck(langGuess, lang));
   });
 })
